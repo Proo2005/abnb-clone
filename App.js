6443f3ea7641400864145bdcc2e450c2ddbc9ab9
Prodipta chakraborty <124602243+Proo2005@users.noreply.github.com>
2025-06-16 23:59:59 +0530

    Add files via upload
    stayfinder 
    
    
    npx create-react-app {foldername}
    npm install react-router-dom
    npm install react-icons
    npm install axios
    npm start

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/login';
+import Register from './pages/Register';
+import Book from './pages/book';
+import Bookings from './pages/Bookings';
+
+
+// Add other routes for other pages like PropertyDetails, SearchResults when ready
+// Add other imports like PropertyDetails, SearchResults when ready
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/book" element={<Book />} />
+        <Route path="/bookings" element={<Bookings />} />
+
+
+        {/* <Route path="/property/:id" element={<PropertyDetails />} /> */}
+        {/* <Route path="/search" element={<SearchResults />} /> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
